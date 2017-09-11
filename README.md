1c80028b924e72632f29e0e43ce8f901ebaf0448
runner <dingchunkun_2006@126.com>
2017-09-11 23:48:22 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # scm-platform
+
+
+* scm-platform-dubbo-api 
+* scm-platform-dubbo-provider
+* - scm-platform-dubbo-provider-backend
+* - scm-platform-dubbo-provider-frontend
+* scm-platform-dubbo-consumer
+* - scm-platform-dubbo-consumer-backend
+* - scm-platform-dubbo-consumer-frontend
+
+* scm-platform-dubbo-web
+* scm-platform-common
+* scm-platform-dao
+* scm-platform-parent
+* scm-platform-admin
+* scm-platform-app
+* 
+
--- scm-platform-admin/src/main/java/com/scm/platform/config/RedisCacheConfiguration.java
+++ scm-platform-admin/src/main/java/com/scm/platform/config/RedisCacheConfiguration.java
@@ -1,8 +1,102 @@
 package com.scm.platform.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
 /**
- * @author dingc
- * @date 2017/9/11.
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *                  十年生死两茫茫，写程序，到天亮。
+ *                      千行代码，Bug何处藏。
+ *                  纵使上线又怎样，朝令改，夕断肠。
+ *                  领导每天新想法，天天改，日日忙。
+ *                      相顾无言，惟有泪千行。
+ *                  每晚灯火阑珊处，夜难寐，加班狂。
  */
-public class RedisCacheConfiguration {
+@Configuration
+@EnableCaching
+public class RedisCacheConfiguration extends CachingConfigurerSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisCacheConfiguration.class);
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+        redisConnectionFactory.setHostName(env.getProperty("spring.redis.host"));
+        redisConnectionFactory.setPort(Integer.parseInt(env.getProperty("spring.redis.port")));
+        return redisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(cf);
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        cacheManager.setDefaultExpiration(600);
+        return cacheManager;
+    }
+
+    public CacheErrorHandler errorHandler() {
+        return new CacheErrorHandler(){
+
+            @Override
+            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+                logger.warn("handleCacheGetError in redis: {}", exception.getMessage());
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+                logger.warn("handleCachePutError in redis: {}", exception.getMessage());
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+                logger.warn("handleCacheEvictError in redis: {}", exception.getMessage());
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+                logger.warn("handleCacheClearError in redis: {}", exception.getMessage());
+            }};
+    }
+
 }
